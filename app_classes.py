7ba9fb40ee5a8d696a9ab394d93b0bdfce85d9c8
Danylo <149315521+dantssm@users.noreply.github.com>
2024-03-11 11:27:10 +0200

    Delete app_classes.py
    we don't need that

--- app_classes.py
+++
@@ -1,9 +0,0 @@
-class User:
-    """Class User"""
-    main_test_answers = []
-    def __init__(self, username, email, password):
-        self.email = email
-        self.username = username
-        self.password = password
-        self.reviewed_wines = []
-        self.wine_rating = {}
